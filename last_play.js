62cba328f5baaa7f14de39e8ec16cc9502e71a5d
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 19:07:45 +0900

    Update last_play.js

--- last_play.js
+++ last_play.js
@@ -1,1 +1,5 @@
-alert("Hello World");
+var xmlHttp=new XMLHttpRequest();
+xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/music_list.php",false);
+xmlHttp.send(null);
+var data=JSON.parse(xmlHttp.responseText);
+console.log(data);
