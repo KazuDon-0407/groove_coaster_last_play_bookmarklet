93242f440988e513e0223c9518dcf43fd1a03bf7
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 20:24:23 +0900

    Update last_play.js

--- last_play.js
+++ last_play.js
@@ -9,11 +9,16 @@ else{
     xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/music_list.php",false);
     xmlHttp.send(null);
     var data=JSON.parse(xmlHttp.responseText);
-    var last_play_time;
+    var last_play_time,music_title;
     for(var i=0;i<data.music_list.length;i++){
-       if(data.music_list[i].music_id==music_id) last_play_time=data.music_list[i].last_play_time;
+       if(data.music_list[i].music_id==music_id){
+           last_play_time=data.music_list[i].last_play_time;
+           music_title=data.music_list[i].music_title;
+           break;
+       }
     }
     var alert_disp="";
+    alert_disp+=music_title+"\n";
     var now=new Date();
     var last=new Date(last_play_time);
     var diffTime = now.getTime() - last.getTime();
