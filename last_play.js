f18388e8dc6356cbc239ab937bbfbb9b61162c20
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 19:18:38 +0900

    Update last_play.js

--- last_play.js
+++ last_play.js
@@ -4,4 +4,5 @@ xmlHttp.send(null);
 var data=JSON.parse(xmlHttp.responseText);
 var newer_time_str=data.music_list[0].last_play_time;
 //var older_time_str=data.music_list[data.length-1].last_play_time;
+console.log(data);
 console.log(typeof(newer_time_str));
