6ab3f4a427e00ec9f77c560dfa9c6f2cc4ff24ec
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-14 19:35:20 +0900

    Update last_play.js

--- last_play.js
+++ last_play.js
@@ -1,7 +1,7 @@
 const href=location.href;
 var match=href.match("https://mypage.groovecoaster.jp/sp/#/mc/");
 if(match==null){
-    alert("%E3%83%AA%E3%82%B6%E3%83%AB%E3%83%88%E7%94%BB%E9%9D%A2%E3%81%A7%E5%AE%9F%E8%A1%8C%E3%81%97%E3%81%A6%E3%81%8F%E3%81%A0%E3%81%95%E3%81%84");
+    alert("リザルト画面で実行してください");
 }
 else{
     const music_id=href.replace(/[^0-9]/g,'');
@@ -14,6 +14,7 @@ else{
     for(var i=0;i<data.music_list.length;i++){
        if(data.music_list[i].music_id==music_id) last_play_time=data.music_list[i].last_play_time;
     }
-    var alert_disp="Hello World";
+    var alert_disp="";
+    alert_disp+="最終プレイ日時:"+last_play_time;
     alert(alert_disp);
 }
