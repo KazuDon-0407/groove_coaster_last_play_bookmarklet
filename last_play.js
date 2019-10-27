c1e209e58719db5054225d9e40fecc9d3e7fc686
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-27 22:27:29 +0900

    Update last_play.js

--- last_play.js
+++ last_play.js
@@ -26,5 +26,5 @@ else{
     alert_disp+="最終プレイ日時:"+last_play_time+"\n";
     if(diffDay<1) alert_disp+="24時間以内にプレイしました";
     else alert_disp+=diffDay+"日前にプレイしました";
-    alert(alert_disp+"b");
+    alert(alert_disp);
 }
