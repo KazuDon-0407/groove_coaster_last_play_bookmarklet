33288c9d9d173b3d26e099c8bdac730065c0ffb8
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2019-10-27 22:22:55 +0900

    Update last_play.js

--- last_play.js
+++ last_play.js
@@ -1,13 +1,12 @@
 const href=location.href;
 var match=href.match("https://mypage.groovecoaster.jp/sp/#/mc/");
-if(match!=null){
+if(match==null){
     alert("リザルト画面で実行してください");
 }
 else{
-    //const music_id=href.replace(/[^0-9]/g,'');
-     const music_id=438;
+    const music_id=href.replace(/[^0-9]/g,'');
     var xmlHttp=new XMLHttpRequest();
-    xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/friend_music_list.php?hash=7a80ace43a59313f004c8da2a3c982fdaeb2853ea0209e8145532093206825cd",false);
+    xmlHttp.open("GET","https://mypage.groovecoaster.jp/sp/json/music_list.php",false);
     xmlHttp.send(null);
     var data=JSON.parse(xmlHttp.responseText);
     var last_play_time,music_title;
@@ -24,9 +23,8 @@ else{
     var last=new Date(last_play_time);
     var diffTime = now.getTime() - last.getTime();
     var diffDay = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-    console.log(diffDay);
     alert_disp+="最終プレイ日時:"+last_play_time+"\n";
-    if(diffDay==0) alert_disp+="24時間以内にプレイしました";
+    if(diffDay<1) alert_disp+="24時間以内にプレイしました";
     else alert_disp+=diffDay+"日前にプレイしました";
     alert(alert_disp);
 }
